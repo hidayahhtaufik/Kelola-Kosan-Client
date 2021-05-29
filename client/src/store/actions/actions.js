c376ca9f4b62e43ce74e71300dbb7db48a85a7d3
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-05-29 21:16:55 +0700

    make Tenant store and try with json server

--- client/src/store/actions/actions.js
+++ client/src/store/actions/actions.js
@@ -1,6 +1,7 @@
-const RevenueDB = 'http://localhost:3001/revenue';
-const RoomDB = 'http://localhost:3001/room';
-import axios from '../../API/axios' // ganti pake axios
+const RevenueDB = 'http://localhost:4000/revenue';
+const RoomDB = 'http://localhost:4000/room';
+const TenantDB = 'http://localhost:4000/tenant';
+// import axios from '../../API/axios'; // ganti pake axios
 
 export const fetchRevenue = () => {
   return (dispatch) => {
@@ -23,6 +24,20 @@ export const fetchRoom = () => {
       .then((data) => {
         console.log(data, '<<<< di Action Room');
         return dispatch({ type: 'ROOM/FETCH', payload: data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+};
+
+export const fetchTenant = () => {
+  return (dispatch) => {
+    fetch(TenantDB, { method: 'GET' })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data, '<<<< di Action Room');
+        return dispatch({ type: 'TENANT/FETCH', payload: data });
       })
       .catch((err) => {
         console.log(err);
--- client/src/store/reducers/reducer.js
+++ client/src/store/reducers/reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import revenueReducer from './revenue.reducer';
 import roomReducer from './room.reducer';
+import tenantReducer from './tenant.reducer';
 
 const rootReducer = combineReducers({
   revenue: revenueReducer,
   room: roomReducer,
+  tenant: tenantReducer,
 });
 
 export default rootReducer;
