afb548d1654016d938badc1210d4baa63cf0b9b0
ramacahya <rama.cahya.93@gmail.com>
2021-05-30 19:30:34 +0700

    config with redux devtool extension for chrome, slicing table tenants

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,6 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 function logger() {
   return function (next) {
@@ -10,6 +12,6 @@ function logger() {
   };
 }
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 export default store;
