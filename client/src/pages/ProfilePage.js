ce7f54da84490dc94455502db5bb9278c408684c
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-06-01 08:44:23 +0700

    styling profile and adding pdf download in table expense

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -94,98 +94,101 @@ function ProfilePage() {
                   Property Profile
                 </h1>
               </Row>
-              <Row
-                className='mb-2'
-                style={{
-                  // border: 'solid',
-                  // borderColor: 'yellow',
-                  padding: '20px',
-                  backgroundColor: '#f54748',
-                  borderTopRightRadius: '30px',
-                  borderBottomLeftRadius: '30px',
-                }}
-              >
-                <Col>
-                  <h3 className='text-center' style={{ color: 'white' }}>
-                    {property[0]?.name}
-                  </h3>
-                </Col>
-              </Row>
-              <Row
-                style={{
-                  // border: 'solid',
-                  // borderColor: 'green',
-                  padding: '20px',
-                  backgroundColor: 'white',
-                  borderRadius: 30,
-                }}
-              >
-                <Col
+              <div style={{ padding: '20px' }}>
+                <Row
+                  className='mb-4 mt-2 shadow'
                   style={{
-                    border: 'solid',
-                    borderColor: 'purple',
+                    // border: 'solid',
+                    // borderColor: 'yellow',
                     padding: '20px',
+                    backgroundColor: '#f54748',
+                    borderTopRightRadius: '30px',
+                    borderBottomLeftRadius: '30px',
                   }}
                 >
-                  <div className='text-center mt-3'>
-                    <Image
-                      src={property[0]?.image}
-                      className='rounded'
-                      alt='Kamar Kosan'
-                      style={{ width: '100%' }}
-                      rounded
-                    />
-                  </div>
-                </Col>
-                <Col
-                  className='align-items-center justify-content-center'
+                  <Col>
+                    <h3 className='text-center' style={{ color: 'white' }}>
+                      {property[0]?.name}
+                    </h3>
+                  </Col>
+                </Row>
+                <Row
+                  className='shadow'
                   style={{
-                    border: 'solid',
-                    borderColor: 'gray',
+                    // border: 'solid',
+                    // borderColor: 'green',
                     padding: '20px',
+                    backgroundColor: 'white',
+                    borderRadius: 30,
                   }}
                 >
-                  <Table bordered hover>
-                    <tbody>
-                      <tr>
-                        <td>Name Owner:</td>
-                        <td>{property[0]?.username}</td>
-                      </tr>
-                    </tbody>
-                    <tbody>
-                      <tr>
-                        <td className={styles.column}>Address:</td>
-                        <td>{property[0]?.address}</td>
-                      </tr>
-                    </tbody>
-                    <tbody>
-                      <tr>
-                        <td>Phone Number:</td>
-                        <td>{property[0]?.phone}</td>
-                      </tr>
-                    </tbody>
-                    <tbody>
-                      <tr>
-                        <td>Email:</td>
-                        <td>{property[0]?.email}</td>
-                      </tr>
-                    </tbody>
-                    <tbody>
-                      <tr>
-                        <td>Quantity Room:</td>
-                        <td>{roomData.length} Room</td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                  <Button
-                    variant='success rounded'
-                    className='mr-5'
-                    onClick={handleShow}
+                  <Col
+                    style={{
+                      // border: 'solid',
+                      // borderColor: 'purple',
+                      padding: '20px',
+                    }}
                   >
-                    Edit data
-                  </Button>
-                </Col>
-              </Row>
+                    <div className='text-center shadow mt-3'>
+                      <Image
+                        src={property[0]?.image}
+                        className='rounded'
+                        alt='Kamar Kosan'
+                        style={{ width: '100%' }}
+                        rounded
+                      />
+                    </div>
+                  </Col>
+                  <Col
+                    className='align-items-center justify-content-center'
+                    style={{
+                      // border: 'solid',
+                      // borderColor: 'gray',
+                      padding: '20px',
+                    }}
+                  >
+                    <Table bordered hover>
+                      <tbody>
+                        <tr>
+                          <td>Name Owner:</td>
+                          <td>{property[0]?.fullname}</td>
+                        </tr>
+                      </tbody>
+                      <tbody>
+                        <tr>
+                          <td className={styles.column}>Address:</td>
+                          <td>{property[0]?.address}</td>
+                        </tr>
+                      </tbody>
+                      <tbody>
+                        <tr>
+                          <td>Phone Number:</td>
+                          <td>{property[0]?.phone}</td>
+                        </tr>
+                      </tbody>
+                      <tbody>
+                        <tr>
+                          <td>Email:</td>
+                          <td>{property[0]?.email}</td>
+                        </tr>
+                      </tbody>
+                      <tbody>
+                        <tr>
+                          <td>Quantity Room:</td>
+                          <td>{roomData.length} Room</td>
+                        </tr>
+                      </tbody>
+                    </Table>
+                    <Button
+                      variant='success rounded'
+                      className='mr-5'
+                      onClick={handleShow}
+                    >
+                      Edit data
+                    </Button>
+                  </Col>
+                </Row>
+              </div>
             </Col>
           )}
         </Row>
